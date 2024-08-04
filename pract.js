03753f36759b2625705beaad1b6a1b8d61fd6b7b
Rohit Chandel <149170278+RohitChandel23@users.noreply.github.com>
2024-08-04 19:30:42 +0530

    Update pract.js

--- pract.js
+++ pract.js
@@ -1,13 +1,1 @@
-const ob = document.querySelector("#photo");
-const hel = document.getElementById("headi");
-const p = document.querySelector("#para")
-
-p.innerText="ttttttttttt"
-hel.innerText="rom rom";
-
-
-
-
-ob.src="images/clouds.png"
-
 
